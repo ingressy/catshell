374a9dbe0ef56e3b12cea7b79bb498a0a34bb907
ingressy <j.czinzoll@ingressy.de>
2024-08-22 21:07:02 +0200

    Create help.py

---
+++ help.py
@@ -0,0 +1,8 @@
+class help:
+    def help_txt():
+        print("~~{help}~~")
+        print("help -> Zeigt dir alle möglichen Befehle und eine Erklärung dieser an.")
+        print("info -> Zeigt dir Infomationen über das Programm an.")
+        print("ls   -> Zeigt dir den Ordner an in den du grade bist.")
+        print("stop -> Dieser Befehl darf nie ausgeführt werden! Nie!!")
+        print("~~{help}~~")
